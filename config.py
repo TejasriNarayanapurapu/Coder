0aff296206ebeefec0621fb57f634e7960d8edd2
Tejasri Narayanapurapu <naryanapurapusiva@gmail.com>
2025-07-31 18:23:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-GITHUB_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
-
-print("Loaded GitHub Token:", GITHUB_TOKEN[:4] + "..." + GITHUB_TOKEN[-4:] if GITHUB_TOKEN else "Token not loaded!")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
